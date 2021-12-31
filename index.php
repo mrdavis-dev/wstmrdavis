d5caae365722f0edebd89caf29599e48718f9285
David Flores <31556253+mrdavis-dev@users.noreply.github.com>
2021-12-31 13:04:48 -0500

    i

--- index.php
+++ index.php
@@ -2,19 +2,35 @@
 
 include("conn.php");
 
-$user = $_GET['username'];
-$pass = $_GET['password'];
+$usuario = $_POST['user'];
+$password = $_POST['pass'];
+// $options = array("cost" => 4);
+// $hashPassword = password_hash($password, PASSWORD_BCRYPT, $options);
 
-$sql = "SELECT username, password FROM users WHERE username = '$user' AND password = '$pass'";
-// $sql = "SELECT nombre, apellido FROM cobradores WHERE nombre = '$user' AND apellido = '$pass'";
-$result = $db->query($sql);
-if ($result->num_rows > 0) {
-    // output data of each row
-    while ($row = $result->fetch_assoc()) {
-        echo json_encode("ok");
-        // echo 'ok';
-    }
-} else {
-    echo json_encode('contraseña o usuario incorrecto');
-    // echo '<script>console.log("error")</script>';
+$consulta = $link->prepare("SELECT * FROM users WHERE username=? AND password=?");
+$consulta->bind_param('ss',$usuario,$password);
+$consulta->execute();
+
+$result = $consulta->get_result();
+if($res = $result->fetch_assoc()){
+    echo json_encode($res,JSON_UNESCAPED_UNICODE);
 }
+
+$consulta->close();
+$link->close();
+// $user = $_GET['username'];
+// $pass = $_GET['password'];
+
+// $sql = "SELECT username, password FROM users WHERE username = '$user' AND password = '$pass'";
+// // $sql = "SELECT nombre, apellido FROM cobradores WHERE nombre = '$user' AND apellido = '$pass'";
+// $result = $db->query($sql);
+// if ($result->num_rows > 0) {
+//     // output data of each row
+//     while ($row = $result->fetch_assoc()) {
+//         echo json_encode("ok");
+//         // echo 'ok';
+//     }
+// } else {
+//     echo json_encode('contraseña o usuario incorrecto');
+//     // echo '<script>console.log("error")</script>';
+// }
